c7c375d5e65dbb16074a7e1edfb7a4a96fee1f62
vdo <vdo@greyfaze.net>
2019-05-22 14:47:25 +0200

    Add wait loop for files

--- build/init.sh
+++ build/init.sh
@@ -18,22 +18,38 @@
 ##           -> GET A MEW ACCOUNT WITH RAIDEN_KEYSTORE_PASSWORD AND LOAD RAIDEN_ADDRESS, RAIDEN_KEYSTORE_PASSWORD_PATH
 ########################################################################################################################
 
-if [ "$RAIDEN_KEYSTORE_PASSWORD" == '' ]; then 
+wait_for() {
+    while [ ! -f "$1" ];do
+        sleep 2
+    done
+}
+
+print_banner() {
+    #### TODO link to the documentation
+    echo
+    echo "########################################################"
+    echo "## Raiden not configured to start!                    ##"
+    echo "########################################################"
+}
+
+# Create keystore dir to allow uploads
+mkdir -p "/root/.raiden/keystore"
+
+if [ "${RAIDEN_KEYSTORE_PASSWORD}" == '' ]; then 
     if [ -z "${RAIDEN_ADDRESS}" ]; then
-        while read ADDRESS; do
-            if [ -f /root/.raiden/keystore/.password_$ADDRESS ];then
+        while read -r ADDRESS; do
+            if [ -f "/root/.raiden/keystore/.password_$ADDRESS" ];then
                 export RAIDEN_ADDRESS=${ADDRESS/ /}
                 export RAIDEN_KEYSTORE_PASSWORD_PATH=/root/.raiden/keystore/.password_${ADDRESS}
                 export RAIDEN_KEYSTORE_PASSWORD=$(cat /root/.raiden/keystore/.password_${ADDRESS})
                 break
             fi
         done <<< "$(find /root/.raiden/keystore -maxdepth 1 -type f -not -name ".*" -exec basename \{} \;)"
-        # Create a new address and password
         if [ -z "${RAIDEN_ADDRESS}" ];then
             export RAIDEN_KEYSTORE_PASSWORD=$(echo $(LC_CTYPE=C tr -dc 'A-HJ-NPR-Za-km-z2-9' < /dev/urandom | head -c 20))
-            onboarder -p ${RAIDEN_KEYSTORE_PASSWORD}
-            while read ADDRESS; do
-                if [ -f /root/.raiden/keystore/.password_$ADDRESS ];then
+            onboarder -p "${RAIDEN_KEYSTORE_PASSWORD}"
+            while read -r ADDRESS; do
+                if [ -f "/root/.raiden/keystore/.password_$ADDRESS" ];then
                     export RAIDEN_ADDRESS=$ADDRESS
                     export RAIDEN_KEYSTORE_PASSWORD_PATH=/root/.raiden/keystore/.password_$ADDRESS
                     export RAIDEN_KEYSTORE_PASSWORD=$(cat /root/.raiden/keystore/.password_${ADDRESS})
@@ -43,56 +59,63 @@ if [ "$RAIDEN_KEYSTORE_PASSWORD" == '' ]; then
         fi
     else
         RAIDEN_ADDRESS=$(to_checksum_address -a ${RAIDEN_ADDRESS})
-        EXISTS=$(grep -qi ${RAIDEN_ADDRESS/0x/} /root/.raiden/keystore/*)
+        EXISTS=$(grep -qi "${RAIDEN_ADDRESS/0x/}" /root/.raiden/keystore/*)
         RESULT=$?
-        if [ ${RESULT} -eq 0 ] && [ -f /root/.raiden/keystore/.password_${RAIDEN_ADDRESS} ];then
+        if [ ${RESULT} -eq 0 ] && [ -f "/root/.raiden/keystore/.password_${RAIDEN_ADDRESS}" ];then
             export RAIDEN_KEYSTORE_PASSWORD_PATH="/root/.raiden/keystore/.password_${RAIDEN_ADDRESS}"
             export RAIDEN_KEYSTORE_PASSWORD=$(cat ${RAIDEN_KEYSTORE_PASSWORD_PATH})
         else
             if [ ${RESULT} -ne 0 ];then
-                echo "the account ${RAIDEN_ADDRESS} file could not be found"
-                ### TODO wait until user upload .password file
-                ### add instructions or link
-            elif [ ! -f /root/.raiden/keystore/.password_${RAIDEN_ADDRESS} ];then
-                echo "the password file for the account ${RAIDEN_ADDRESS} could not be found"
-                ### TODO wait until user upload .password file and load parameters
-                ### add instructions or link
+                print_banner
+                echo "* The account ${RAIDEN_ADDRESS} file could not be found."
+                echo "* Upload it with the filename ${RAIDEN_ADDRESS} to the path:"
+                echo "  /root/.raiden/keystore/"
+                wait_for "/root/.raiden/keystore/${RAIDEN_ADDRESS}"
+            elif [ ! -f "/root/.raiden/keystore/.password_${RAIDEN_ADDRESS}" ];then
+                print_banner
+                echo "* The password file for the account ${RAIDEN_ADDRESS} could not be found"
+                echo "* Upload it with the filename .password_${RAIDEN_ADDRESS} to the path:"
+                echo " /root/.raiden/keystore/"
+                wait_for "/root/.raiden/keystore/.password_${RAIDEN_ADDRESS}"
+                export RAIDEN_KEYSTORE_PASSWORD_PATH="/root/.raiden/keystore/.password_${RAIDEN_ADDRESS}"
+                export RAIDEN_KEYSTORE_PASSWORD=$(cat ${RAIDEN_KEYSTORE_PASSWORD_PATH})
             fi
         fi
     fi
 else
     if [ -n "${RAIDEN_ADDRESS}" ]; then
-        echo $RAIDEN_KEYSTORE_PASSWORD > /root/.raiden/keystore/.password_${RAIDEN_ADDRESS}
+        echo "$RAIDEN_KEYSTORE_PASSWORD" > "/root/.raiden/keystore/.password_${RAIDEN_ADDRESS}"
         RAIDEN_KEYSTORE_PASSWORD_PATH="/root/.raiden/keystore/.password_${RAIDEN_ADDRESS}"
-        RAIDEN_ADDRESS=$(to_checksum_address -a ${RAIDEN_ADDRESS})
-        EXISTS=$(grep -qi ${RAIDEN_ADDRESS/0x/} /root/.raiden/keystore/*)
+        RAIDEN_ADDRESS=$(to_checksum_address -a "${RAIDEN_ADDRESS}")
+        EXISTS=$(grep -qi "${RAIDEN_ADDRESS/0x/}" /root/.raiden/keystore/*)
         RESULT=$?
         if [ ${RESULT} -eq 0 ];then
             export RAIDEN_KEYSTORE_PASSWORD_PATH="/root/.raiden/keystore/.password_${RAIDEN_ADDRESS}"
-            export RAIDEN_KEYSTORE_PASSWORD=$(cat ${RAIDEN_KEYSTORE_PASSWORD_PATH})
+            export RAIDEN_KEYSTORE_PASSWORD=$(cat "${RAIDEN_KEYSTORE_PASSWORD_PATH}")
         else
             if [ ${RESULT} -ne 0 ];then
-                echo "the account ${RAIDEN_ADDRESS} file could not be found"
-                exit 1
-                ### TODO wait until user upload .password file
-                ### add instructions or link
+                print_banner
+                echo "* The account ${RAIDEN_ADDRESS} file could not be found."
+                echo "* Upload it with the filename ${RAIDEN_ADDRESS} to the path:"
+                echo "  /root/.raiden/keystore/"
+                wait_for "/root/.raiden/keystore/${RAIDEN_ADDRESS}"
             fi
         fi
     else
-        EXISTS=$(grep -il ${RAIDEN_KEYSTORE_PASSWORD} /root/.raiden/keystore/.*)
+        EXISTS=$(grep -il "${RAIDEN_KEYSTORE_PASSWORD}" /root/.raiden/keystore/.*)
         RESULT=$?
         if [ ${RESULT} -eq 0 ];then
-            export RAIDEN_ADDRESS=$(echo $EXISTS | awk -F "_" '{print $2}')
-            export RAIDEN_KEYSTORE_PASSWORD_PATH=$EXISTS
-            export RAIDEN_KEYSTORE_PASSWORD=$(cat ${RAIDEN_KEYSTORE_PASSWORD_PATH})
+            export RAIDEN_ADDRESS=$(echo "${EXISTS}" | awk -F "_" '{print $2}')
+            export RAIDEN_KEYSTORE_PASSWORD_PATH=${EXISTS}
+            export RAIDEN_KEYSTORE_PASSWORD=$(cat "${RAIDEN_KEYSTORE_PASSWORD_PATH}")
         else
-            onboarder -p ${RAIDEN_KEYSTORE_PASSWORD}
-            EXISTS=$(grep -il ${RAIDEN_KEYSTORE_PASSWORD} /root/.raiden/keystore/.*)
+            onboarder -p "${RAIDEN_KEYSTORE_PASSWORD}"
+            EXISTS=$(grep -il "${RAIDEN_KEYSTORE_PASSWORD}" /root/.raiden/keystore/.*)
             RESULT=$?
             if [ ${RESULT} -eq 0 ];then
-                export RAIDEN_ADDRESS=$(echo $EXISTS | awk -F "_" '{print $2}')
-                export RAIDEN_KEYSTORE_PASSWORD_PATH=$EXISTS
-                export RAIDEN_KEYSTORE_PASSWORD=$(cat ${RAIDEN_KEYSTORE_PASSWORD_PATH})
+                export RAIDEN_ADDRESS=$(echo "${EXISTS}" | awk -F "_" '{print $2}')
+                export RAIDEN_KEYSTORE_PASSWORD_PATH=${EXISTS}
+                export RAIDEN_KEYSTORE_PASSWORD=$(cat "${RAIDEN_KEYSTORE_PASSWORD_PATH}")
             fi
         fi
     fi
@@ -100,14 +123,10 @@ fi
 
 # Check if password is set, and at least one file has been uploaded
 if [ -n "${RAIDEN_ADDRESS}" ] && [ -n "${RAIDEN_KEYSTORE_PASSWORD}" ]; then
-    raiden --keystore-path /root/.raiden/keystore --accept-disclaimer --password-file ${RAIDEN_KEYSTORE_PASSWORD_PATH}
+    raiden --keystore-path /root/.raiden/keystore --accept-disclaimer --password-file "${RAIDEN_KEYSTORE_PASSWORD_PATH}"
         while true; do sleep 5; done
 
 else
-    #### TODO link to the documentation
-    echo
-    echo "########################################################"
-    echo "## Raiden not configured to start!                    ##"
-    echo "########################################################"
+    print_banner
     while true; do sleep 5; done
 fi
